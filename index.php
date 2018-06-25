add5c8cf69d13843ff1e8f892a9ef635ca976a77
plojyon <yon.ploj@gmail.com>
2018-06-25 23:20:03 +0200

    fixed the sketch include directory

--- index.php
+++ index.php
@@ -39,13 +39,13 @@
 				// this is used to prevent XSS (via the use of "../../fishy_stuff.exe")
 			}
 			// no such sketch found (404)
-			elseif (!file_exists($_GET['sketch'])) {
+			elseif (!file_exists('sketches/'.$_GET['sketch'].'.js')) {
 					report("Sketch does not exist!", "The selected sketch (".$_GET['sketch'].") was not found.");
 			}
 			// all OK
 			else {
 				// print the <script> include tag to load the sketch javascript
-				echo '<script src="'.$_GET['sketch'].'.js"></script>';
+				echo '<script src="sketches/'.$_GET['sketch'].'.js"></script>';
 			}
 
 
