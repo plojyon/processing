9691a9c38f3373c5776469563ec005375a059d96
plojyon <yon.ploj@gmail.com>
2018-06-25 22:04:42 +0200

    styled "no sketch selected" error message

--- index.php
+++ index.php
@@ -10,16 +10,30 @@
 	    <title></title>
 		<!--<script language="javascript" type="text/javascript" src="libraries/p5.js"></script>
 		<script language="javascript" type="text/javascript" src="sketch_180625a.js"></script>-->
-	    <style></style>
+	    <style>
+			body {
+				font-family: sans-serif;
+			}
+			.error {
+				color: red;
+			}
+			.error.container {
+				background-color: pink;
+				border: 1px solid red;
+				padding: 1em;
+			}
+		</style>
 	</head>
 	<body>
 		<!-- TODO: Content here -->
-		<p>Stuff comes here.</p>
 		<?php
-			if (!isset($_GET['sketch']))
-				echo "No sketch selected!\nReturn to the main page and select a sketch. If you encounter a broken link, please contact me at <a href='mailto:yon.ploj@gmail.com'>yon.ploj@gmail.com</a>";
+			if (!isset($_GET['sketch'])) // a sketch was not selected in the parameter
+				// print the error message "No sketch selected!" (with styling)
+				echo "<div class='error container'><h1 class='error title'>ERROR: No sketch selected!</h1><p class='error content'>Return to the main page and select a sketch. If you encounter a broken link, please contact me at <a href='mailto:yon.ploj@gmail.com'>yon.ploj@gmail.com</a></p></div>";
 			else
+				// an OK sketch was selected, display it
 				include('sketches/'.$_GET['sketch']);
+				// TODO: check if sketch exists, and prevent ".." in the sketch name (to prevent XSS)
 		?>
 	</body>
 </html>
