661b4e69b35b33a94b8b78fa11172a3e10fe6b26
plojyon <yon.ploj@gmail.com>
2018-06-25 21:53:10 +0200

    added basic PHP logic

--- index.php
+++ index.php
@@ -10,8 +10,10 @@
 		<!-- TODO: Content here -->
 		<p>Stuff comes here.</p>
 		<?php
-			echo "Testing PHP on github...\n";
-			echo "Username: ".$_GET['uname']."\n";
+			if (!isset($_GET['sketch']))
+				echo "No sketch selected!\nReturn to the main page and select a sketch. If you encounter a broken link, please contact me at <a href='mailto:yon.ploj@gmail.com'>yon.ploj@gmail.com</a>";
+			else
+				include('sketches/'.$_GET['sketch']);
 		?>
 	</body>
 </html>
