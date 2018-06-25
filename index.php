242683b6182d98da33de4c5eff16b6e8beac117e
plojyon <yon.ploj@gmail.com>
2018-06-25 18:46:35 +0200

    added p5.js and PHP test

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+	    <title></title>
+		<!--<script language="javascript" type="text/javascript" src="libraries/p5.js"></script>
+		<script language="javascript" type="text/javascript" src="sketch_180625a.js"></script>-->
+	    <style></style>
+	</head>
+	<body>
+		<!-- TODO: Content here -->
+		<p>Stuff comes here.</p>
+		<?php
+			echo "Testing PHP on github...";
+		?>
+	</body>
+</html>
