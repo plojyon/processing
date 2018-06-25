d5a5859a052161bda46da09dba20c289335439bb
plojyon <yon.ploj@gmail.com>
2018-06-25 21:59:16 +0200

    added cache prevention

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <html>
 	<head>
+		<?php
+			header("Expires: Mon, 4 Jan 1999 12:00:00 GMT"); // Expired already
+			header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT"); // always modified
+			header("Cache-Control: no-cache, must-revalidate"); // good for HTTP/1.1
+			header("Pragma: no-cache"); // good for HTTP/1.0
+		?>
 		<meta charset="UTF-8">
 	    <title></title>
 		<!--<script language="javascript" type="text/javascript" src="libraries/p5.js"></script>
