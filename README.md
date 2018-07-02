ea971d19cad7441afdb5c4b9adee7d5eaeb9450b
plojyon <yon.ploj@gmail.com>
2018-07-02 12:57:57 +0200

    (hopefully) made the server download resources instead

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Processing
-[View project here](http://yon.ddns.net/OTHR/processing/) (hosted partially via Github pages, partially by me)
+**[View project here](http://yon.ddns.net/OTHR/processing/)** (code and data hosted by Github, but executed on my server; link goes to *my* server)
+
+[Github pages link](https://plojyon.github.io/processing/) (nothing to see here)
 
 ---
 
-A repo for all my [processing](https://processing.org/) and [p5](https://p5js.org/) sketches
+A repo for all my [processing](https://processing.org/) and [p5.js](https://p5js.org/) sketches
 
 Credit for inspiration *for probably the vast majority of these* goes to [Daniel Shiffman's "The Coding Train"](http://thecodingtrain.com/CodingChallenges/)
--- index.php
+++ index.php
@@ -93,7 +93,7 @@
 			$site = "https://plojyon.github.io/processing/";
 			$sketches_dir = $site."sketches/";
 			$thumbnail_dir = $site."thumbnails/";
-			$sketches = array("flowfield", "wavy_blocks", "barnsley_fern", "knots");
+			$sketches = array("flowfield", "wavy_blocks", "barnsley_fern", "knots", "shtikmen");
 			$sketch_count = count($sketches);
 
 			// a sketch was NOT selected in the parameter
@@ -120,6 +120,7 @@
 				report("Invalid sketch name", "Sketch names can only contain letters, digits or underscores.");
 				// this is used to prevent XSS (via the use of "../../fishy_stuff.exe")
 			}
+			/*
 			// all OK (found .js)
 			elseif (remote_exists($sketches_dir.$_GET['sketch'].'.js')) {
 				// print the <script> include tag to load the sketch javascript
@@ -142,12 +143,50 @@
 					#sketch-title:after { content: ".pde"; }
 				</style>';
 			}
-			// no such sketch found (404)
-			else {
+			*/
+			elseif (remote_exists($sketches_dir.$_GET['sketch'].'.js'))
+				load_sketch($sketches_dir, $_GET['sketch'], '.js');
+
+			elseif (remote_exists($sketches_dir.$_GET['sketch'].'.pde'))
+				load_sketch($sketches_dir, $_GET['sketch'], '.pde');
+
+			else // no such sketch found (404)
 				report("Sketch does not exist", "The selected sketch (".$_GET['sketch'].") was not found.");
+
+
+			// fetches a sketch from github and overwrites the local copy
+			// then redirects the user to the newly created sketch
+			function load_sketch($dir, $name, $extension) {
+				// 0. if project is already locally stored, delete it
+				if (file_exists('sketches/'.$name))
+					deleteDir('sketches/'.$name);
+				mkdir('sketches/'.$name);
+
+				// 1. generate index.html
+				$index = '<h1 id="sketch-title">'.$name.$extension.'</h1>';
+				if ($extension === ".js")
+					$index .= '<script src="'.$name.'.js"></script>';
+				elseif ($extension === ".pde")
+					$index .= '<canvas data-processing-sources="'.$name.'.pde"></canvas>';
+				$index .= '
+				<style>
+					body { margin: 0; text-align: center; }
+				</style>';
+				file_put_contents('sketches/'.$name.'/index.html', $index);
+
+				// 2. fetch source file from Github
+				$source = file_get_contents($dir.'/'.$name.$extension);
+				file_put_contents('sketches/'.$name.'/'.$name.$extension, $source);
+
+				// 3. TODO: check for additional media files
+				//   3.1 TODO: download additional files, possibly overwriting existing data
+
+				// 4. redirect user to the newly created index.html
+				header('Location: sketches/'.$name.'/index.html');
+				die();
 			}
 
-
+			// echoes a pretty-formated sketch div for the main page (with thumbnail)
 			function add_sketch($sketch, $thumbnail_dir) {
 				echo '
 				<div class="container" onclick="window.location.href = \'?sketch='.$sketch.'\';">
@@ -156,8 +195,7 @@
 				</div>';
 			}
 
-
-			// prints a pretty error report
+			// echoes an error report div
 			function report($title, $content) {
 				echo "
 				<div class='error container'>
@@ -176,9 +214,23 @@
 			}
 
 			// checks if a file exists, but works on remote directories too (http://)
+			// source: StackOverflow (https://stackoverflow.com/questions/2280394/how-can-i-check-if-a-url-exists-via-php)
 			function remote_exists($url) {
 				$headers = get_headers($url);
 				return stripos($headers[0], "200 OK") ? true:false; // return actual boolean
+			}
+
+			// recursively deletes a non-empty directory
+			// source: StackOverflow (https://stackoverflow.com/questions/3349753/delete-directory-with-files-in-it)
+			function deleteDir($dirPath) {
+				if (!is_dir($dirPath)) throw new InvalidArgumentException("$dirPath must be a directory");
+				if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') $dirPath .= '/';
+				$files = glob($dirPath . '*', GLOB_MARK);
+				foreach ($files as $file) {
+					if (is_dir($file)) self::deleteDir($file);
+					else unlink($file);
+				}
+				rmdir($dirPath);
 			}
 		?>
 	</body>
